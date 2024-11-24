6b7a71d06494001257f89356710e33fbdb8935ad
iamageo <geovani.af4@gmail.com>
2024-11-24 15:37:29 -0300

    Initial Commit

---
+++ SwiftUI-ToDoList/SwiftUI_ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ToDoListApp.swift
+//  SwiftUI-ToDoList
+//
+//  Created by Geovani Amaral on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-ToDoListTests/SwiftUI_ToDoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ToDoListTests.swift
+//  SwiftUI-ToDoListTests
+//
+//  Created by Geovani Amaral on 24/11/24.
+//
+
+import Testing
+@testable import SwiftUI_ToDoList
+
+struct SwiftUI_ToDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
